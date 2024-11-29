6f9d4ced35492b741232e4c87f86a2706381c00c
Rve27 <rve27github@gmail.com>
2024-11-29 23:53:02 +0700

    rvkernel-manager: gradle: Update Kotlin plugin to v2.1.0
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/build.gradle.kts
+++ rvkernel-manager/app/build.gradle.kts
@@ -7,8 +7,9 @@ val keystoreProperties = Properties()
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -69,16 +70,19 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity.compose)
 
-    implementation(platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation(libs.androidx.compose.bom)
 
-    implementation("androidx.activity:activity-compose:1.5.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation(libs.androidx.compose.material3)
+
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.androidx.lifecycle.runtime.ktx)
 }
