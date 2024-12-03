589dc13c7e88bfb66b58720345680170af3488e1
Rve27 <rve27github@gmail.com>
2024-12-03 17:58:12 +0700

    rvkernel-manager: Update `kotlinCompilerExtension` to v1.5.15
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/build.gradle.kts
+++ rvkernel-manager/app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.3.2"
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
     kotlinOptions {
