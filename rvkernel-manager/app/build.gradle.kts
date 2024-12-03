71ab045dcc78b1e03c9789292661d53b05d210ad
Rve27 <rve27github@gmail.com>
2024-12-04 00:44:50 +0700

    rvkernel-manager: Update dependencies
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/build.gradle.kts
+++ rvkernel-manager/app/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.devtools.ksp)
 }
 
 android {
@@ -71,18 +72,16 @@ android {
 
 dependencies {
     implementation(libs.androidx.activity.compose)
-
-    implementation(libs.androidx.compose.bom)
-
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
-
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
 
-    implementation(libs.androidx.core.ktx)
-
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 }
