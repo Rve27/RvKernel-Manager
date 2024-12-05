85f1ce021add930ab71f9d4da659660078408cce
Rve27 <rve27github@gmail.com>
2024-12-06 02:16:06 +0700

    rvkernel-manager: Set color for `NoRootDialog`
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/src/main/java/com/rve/rvkernelmanager/utils/RootUtils.kt
+++ rvkernel-manager/app/src/main/java/com/rve/rvkernelmanager/utils/RootUtils.kt
@@ -1,24 +1,10 @@
 package com.rve.rvkernelmanager.utils
 
 import java.io.IOException
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.unit.dp
 
 object RootUtils {
@@ -46,11 +32,13 @@ fun NoRootDialog(onConfirm: () -> Unit) {
             Surface(
                 modifier = Modifier.wrapContentWidth().wrapContentHeight(),
                 shape = MaterialTheme.shapes.large,
-                tonalElevation = AlertDialogDefaults.TonalElevation
+                tonalElevation = AlertDialogDefaults.TonalElevation,
+		color = MaterialTheme.colorScheme.primaryContainer
             ) {
                 Column(modifier = Modifier.padding(16.dp)) {
                     Text(
                         text = "RvKernel Manager requires root access!",
+			color = MaterialTheme.colorScheme.onPrimaryContainer
                     )
                     Spacer(modifier = Modifier.height(24.dp))
                     TextButton(
