53642f514b6d815065d89a32113dc732df30b615
Rve27 <rve27github@gmail.com>
2024-12-06 02:00:05 +0700

    rvkernel-manager: Move `NoRootDialog` to `RootUtils`
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/src/main/java/com/rve/rvkernelmanager/ui/MainActivity.kt
+++ rvkernel-manager/app/src/main/java/com/rve/rvkernelmanager/ui/MainActivity.kt
@@ -4,28 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.AlertDialogDefaults
-import androidx.compose.material3.BasicAlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.rve.rvkernelmanager.ui.screen.HomeScreen
 import com.rve.rvkernelmanager.ui.theme.RvKernelManagerTheme
-import com.rve.rvkernelmanager.utils.RootUtils
+import com.rve.rvkernelmanager.utils.RootUtils.isDeviceRooted
+import com.rve.rvkernelmanager.utils.NoRootDialog
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,8 +15,7 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
 
-        val hasRootAccess = RootUtils.isDeviceRooted()
-        if (!hasRootAccess) {
+        if (!isDeviceRooted()) {
             setContent {
                 RvKernelManagerTheme {
                     NoRootDialog { finish() }
@@ -44,42 +25,6 @@ class MainActivity : ComponentActivity() {
             setContent {
                 RvKernelManagerTheme {
                     HomeScreen()
-                }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun NoRootDialog(onConfirm: () -> Unit) {
-    val openDialog = remember { mutableStateOf(true) }
-
-    if (openDialog.value) {
-        BasicAlertDialog(
-            onDismissRequest = {
-                openDialog.value = false
-            }
-        ) {
-            Surface(
-                modifier = Modifier.wrapContentWidth().wrapContentHeight(),
-                shape = MaterialTheme.shapes.large,
-                tonalElevation = AlertDialogDefaults.TonalElevation
-            ) {
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = "RvKernel Manager requires root access!",
-                    )
-                    Spacer(modifier = Modifier.height(24.dp))
-                    TextButton(
-                        onClick = {
-                            openDialog.value = false
-                            onConfirm()
-                        },
-                        modifier = Modifier.align(Alignment.End)
-                    ) {
-                        Text("Exit")
-                    }
                 }
             }
         }
