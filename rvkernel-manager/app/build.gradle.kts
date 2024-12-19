5e3f330600d83e0612d62f813a17a53f5a2648c2
Rve27 <rve27github@gmail.com>
2024-12-19 20:43:19 +0800

    rvkernel-manager: gradle: Update dependencies
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/build.gradle.kts
+++ rvkernel-manager/app/build.gradle.kts
@@ -92,6 +92,10 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.compose.runtime)
 
     implementation(libs.topjohnwu.libsu.core)
 }
