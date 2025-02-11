71c6525a9b3c9029ad89de30f6949b0a446fc48c
Rve27 <rve27github@gmail.com>
2025-02-11 08:29:39 +0800

    rvkernel-manager: Add animation when GPUCard expanded
    Change-Id: I17d85195643f53648fc8ebba69cc3413cd1ee17e
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/src/main/java/com/rve/rvkernelmanager/ui/screen/SoC.kt
+++ rvkernel-manager/app/src/main/java/com/rve/rvkernelmanager/ui/screen/SoC.kt
@@ -1,10 +1,12 @@
 package com.rve.rvkernelmanager.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.animation.animateContentSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -433,9 +435,13 @@ fun GPUCard(viewModel: SoCViewModel) {
     }
 
     val gpuThrottlingStatus = gpuThrottling == "1"
+    var expanded by remember { mutableStateOf(false) }
 
     Card(
-        shape = CardDefaults.shape
+        shape = CardDefaults.shape,
+	modifier = Modifier
+            .fillMaxWidth()
+            .animateContentSize()
     ) {
         Column(
             modifier = Modifier
@@ -444,7 +450,8 @@ fun GPUCard(viewModel: SoCViewModel) {
         ) {
             Text(
                 text = stringResource(R.string.gpu),
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleLarge,
+		modifier = Modifier.clickable { expanded = !expanded }
             )
             Spacer(Modifier.height(4.dp))
 
