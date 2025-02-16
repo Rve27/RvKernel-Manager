33810489a45d4f0942fdf3d0fcb48b6e974fcde2
Rve27 <rve27github@gmail.com>
2025-02-16 23:44:08 +0700

    rvkernel-manager: Include LeakCanary in debug build
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/build.gradle.kts
+++ rvkernel-manager/app/build.gradle.kts
@@ -105,4 +105,5 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     implementation(libs.topjohnwu.libsu.core)
+    debugImplementation(libs.squareup.leakcanary)
 }
