44de8b03d9025e4bc945c550b88e03ac51cc9671
Rve27 <rve27github@gmail.com>
2025-06-02 22:43:26 +0800

    rvkernel-manager: Refactor signing configuration
    Change-Id: Ie326f0637c40585d3f6ffc6edb89254ad62feea8
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- rvkernel-manager/app/build.gradle.kts
+++ rvkernel-manager/app/build.gradle.kts
@@ -1,10 +1,4 @@
 import java.util.Properties
-import java.io.FileInputStream
-
-val keystorePropertiesFile = rootProject.file("keystore.properties")
-val keystoreProperties = Properties()
-
-keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 plugins {
     alias(libs.plugins.android.application)
@@ -21,10 +15,24 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
+            val propertiesFile = rootProject.file("signing.properties")
+            val properties = if (propertiesFile.exists()) {
+                Properties().apply {
+                    load(propertiesFile.inputStream())
+                }
+            } else {
+                null
+            }
+
+            val getString: (String, String, String) -> String? = { propertyName, environmentName, prompt ->
+                properties?.getProperty(propertyName) ?: System.getenv(environmentName) ?:
+                System.console()?.readLine("\n$prompt: ")
+            }
+
+            storeFile = getString("storeFile", "STORE_FILE", "Store file")?.let { rootProject.file(it) }
+            storePassword = getString("storePassword", "STORE_PASSWORD", "Store password")
+            keyAlias = getString("keyAlias", "KEY_ALIAS", "Key alias")
+            keyPassword = getString("keyPassword", "KEY_PASSWORD", "Key password")
         }
     }
     
@@ -54,7 +62,7 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs["release"]
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
@@ -78,10 +86,6 @@ android {
 	dex {
 	    useLegacyPackaging = true
 	}
-
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
 
         resources.excludes += "DebugProbesKt.bin"
     }
