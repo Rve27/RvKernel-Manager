44130ee014c2baffa733dca4389e726a6869b592
Rve27 <rve27github@gmail.com>
2025-08-03 18:52:33 +0800

    RvKernel-Manager: home: Use `TitleExpandable` component
    Change-Id: I07a6f13ca4d82cecd702dcd09a0658690101ae3f
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/rvkernelmanager/ui/screen/Home.kt
+++ app/src/main/java/com/rve/rvkernelmanager/ui/screen/Home.kt
@@ -3,6 +3,7 @@ package com.rve.rvkernelmanager.ui.screen
 import android.content.*
 import android.net.Uri
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.material.icons.Icons
@@ -24,7 +25,7 @@ import androidx.navigation.NavController
 import com.rve.rvkernelmanager.R
 import com.rve.rvkernelmanager.ui.navigation.*
 import com.rve.rvkernelmanager.ui.viewmodel.HomeViewModel
-import com.rve.rvkernelmanager.ui.component.CustomListItem
+import com.rve.rvkernelmanager.ui.component.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -167,24 +168,32 @@ fun DonateCard() {
 @Composable
 fun CopyrightCard() {
     Card {
-        CustomListItem(
-            title = "License",
+	var isExpanded by rememberSaveable { mutableStateOf(false) }
+
+        TitleExpandable(
+	    leadingIcon = painterResource(R.drawable.ic_license),
+            text = "License",
 	    titleLarge = true,
-            icon = painterResource(R.drawable.ic_license)
+            trailingIcon = if (isExpanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+	    onClick = { isExpanded = !isExpanded }
         )
 
-        HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
+	AnimatedVisibility(isExpanded) {
+	    Column {
+	        HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
 
-        CustomListItem(
-            summary = """
-                Copyright (C) 2025 Rve
+	        CustomListItem(
+	            summary = """
+	                Copyright (C) 2025 Rve
 
-                This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+	                This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-                This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	                This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-                You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
-            """.trimIndent()
-        )
+	                You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+	            """.trimIndent()
+	        )
+	    }
+	}
     }
 }
