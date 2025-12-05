d9ac32a7560245e8c58b19be2aeaac610f43fbf2
Rve27 <rve27github@gmail.com>
2025-12-06 01:22:39 +0700

    MainActivity: Refactor root check UI and handling
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/rvkernelmanager/ui/MainActivity.kt
+++ app/src/main/java/com/rve/rvkernelmanager/ui/MainActivity.kt
@@ -10,8 +10,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ContainedLoadingIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,52 +27,44 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.isRoot
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.composables.core.rememberDialogState
+import com.rve.rvkernelmanager.ui.components.DialogUnstyled
 import com.rve.rvkernelmanager.ui.navigation.RvKernelManagerNavHost
 import com.rve.rvkernelmanager.ui.theme.RvKernelManagerTheme
 import com.topjohnwu.superuser.Shell
+import kotlinx.coroutines.delay
 import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
-  
-    private var isRoot = false
-    
-    private var showRootDialog by mutableStateOf(false)
-    
-    private var isChecking by mutableStateOf(true)
-
-    private val checkRoot = Runnable {
+    var isRoot by mutableStateOf(false)
+    val checkRoot = Runnable {
         Shell.getShell { shell ->
             isRoot = shell.isRoot
-            if (!isRoot) {
-                showRootDialog = true
-            }
-          
-            isChecking = false
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen: SplashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-
-        splashScreen.setKeepOnScreenCondition { isChecking }
-        
         enableEdgeToEdge()
         Thread(checkRoot).start()
-
         setContent {
             RvKernelManagerTheme {
-                RvKernelManagerApp(showRootDialog = showRootDialog)
+                RvKernelManagerApp()
             }
         }
     }
 
     companion object {
         init {
-        
             @Suppress("DEPRECATION")
             if (Shell.getCachedShell() == null) {
                 Shell.setDefaultBuilder(
@@ -76,40 +75,54 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun RvKernelManagerApp(showRootDialog: Boolean = false) {
-    
-    if (showRootDialog) {
-        AlertDialog(
-            onDismissRequest = { 
-                
-            },
-            title = { Text("Root Access Missing") },
-            text = {
-                Text(
-                    text = "RvKernel Manager requires root access to function properly.",
-                    style = MaterialTheme.typography.bodyLarge,
-                )
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        exitProcess(0)
-                    },
-                    colors = ButtonDefaults.textButtonColors(
-                        contentColor = MaterialTheme.colorScheme.error
-                    )
+    @Preview(showBackground = true)
+    @Composable
+    fun RvKernelManagerApp() {
+        val showRootDialog = rememberDialogState(initiallyVisible = true)
+
+        if (isRoot) {
+            RvKernelManagerNavHost()
+        } else {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.surfaceContainerLow
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
-                    Text("Exit")
+                    ContainedLoadingIndicator()
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "waiting for root access to be granted...",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
                 }
-            },
-        )
-    }
-    
-    // Content utama tetap dirender di belakang dialog
-    Surface {
-        RvKernelManagerNavHost()
+                DialogUnstyled(
+                    state = showRootDialog,
+                    title = {
+                        Text(
+                            text = "Root access required",
+                            style = MaterialTheme.typography.titleMedium
+                        )
+                    },
+                    text = {
+                        Text(
+                            text = "RvKernel Manager requires root access",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    },
+                    confirmButton = {
+                        TextButton(
+                            onClick = { showRootDialog.visible = false }
+                        ) {
+                            Text(text = "Close")
+                        }
+                    },
+                )
+            }
+        }
     }
 }
