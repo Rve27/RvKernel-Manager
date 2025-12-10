1eb7ae2d057a73f78c84b4aec41e42e29fbcc43e
Rve27 <rve27github@gmail.com>
2025-12-10 11:25:10 +0700

    Kernel: Add support for sched_lib_name
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/rvkernelmanager/utils/KernelUtils.kt
+++ app/src/main/java/com/rve/rvkernelmanager/utils/KernelUtils.kt
@@ -23,6 +23,7 @@ object KernelUtils {
 
     const val SCHED_AUTO_GROUP = "/proc/sys/kernel/sched_autogroup_enabled"
     const val PRINTK = "/proc/sys/kernel/printk"
+    const val SCHED_LIB_NAME = "/proc/sys/kernel/sched_lib_name"
 
     const val SCHED_UTIL_CLAMP_MAX = "/proc/sys/kernel/sched_util_clamp_max"
     const val SCHED_UTIL_CLAMP_MIN = "/proc/sys/kernel/sched_util_clamp_min"
