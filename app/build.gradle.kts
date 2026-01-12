13a642fd1afdb5b34cab19be13bf663f2e5eaf15
Rve27 <rve27github@gmail.com>
2026-01-13 04:51:14 +0700

    gradle: Import material symbols
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -178,4 +178,5 @@ dependencies {
     implementation(libs.topjohnwu.libsu.core)
 
     implementation(libs.composables.core)
+    implementation(libs.composables.icons.material.symbols.rounded.filled)
 }
