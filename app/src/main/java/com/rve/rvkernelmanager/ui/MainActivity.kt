e5a95f3ea13a4009b26575ac23ac4581366da146
Rve27 <rve27github@gmail.com>
2026-01-22 03:21:15 +0700

    ui: main: Add loading animation on app startup
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/rvkernelmanager/ui/MainActivity.kt
+++ app/src/main/java/com/rve/rvkernelmanager/ui/MainActivity.kt
@@ -23,27 +23,42 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ContainedLoadingIndicator
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.LinearWavyProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -100,13 +115,83 @@ class MainActivity : ComponentActivity() {
         val scope = rememberCoroutineScope()
 
         var showRootDialog by remember { mutableStateOf(true) }
+        var isLoading by remember { mutableStateOf(true) }
+        var percentageVisible by remember { mutableStateOf(false) }
+        val progressAnim = remember { Animatable(0f) }
+
 
         if (isRoot) {
+            LaunchedEffect(Unit) {
+                percentageVisible = true
+                progressAnim.animateTo(
+                    targetValue = 1f,
+                    animationSpec = tween(
+                        durationMillis = 2000,
+                        easing = FastOutSlowInEasing
+                    )
+                )
+                percentageVisible = false
+                isLoading = false
+            }
+
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.surfaceContainer,
             ) {
-                RvKernelManagerNavHost()
+                Crossfade(
+                    targetState = isLoading,
+                    animationSpec = tween(durationMillis = 1000)
+                ) { loading ->
+                    if (loading) {
+                        Box(
+                            modifier = Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(horizontal = 64.dp),
+                            ) {
+                                val percentage = (progressAnim.value * 100).toInt()
+                                val bias = (progressAnim.value * 2) - 1
+
+                                AnimatedVisibility(
+                                    visible = percentageVisible,
+                                    enter = fadeIn(
+                                        animationSpec = MaterialTheme.motionScheme.slowEffectsSpec()
+                                    ) + slideInVertically(
+                                        initialOffsetY = { it },
+                                        animationSpec = MaterialTheme.motionScheme.slowSpatialSpec()
+                                    ),
+                                    exit = fadeOut(
+                                        animationSpec = MaterialTheme.motionScheme.slowEffectsSpec()
+                                    ) + slideOutVertically(
+                                        targetOffsetY = { it },
+                                        animationSpec = MaterialTheme.motionScheme.slowSpatialSpec()
+                                    )
+                                ) {
+                                    Box(
+                                        modifier = Modifier.fillMaxWidth()
+                                    ) {
+                                        Text(
+                                            text = "$percentage%",
+                                            style = MaterialTheme.typography.titleMedium,
+                                            color = MaterialTheme.colorScheme.primary,
+                                            modifier = Modifier.align(
+                                                BiasAlignment(horizontalBias = bias, verticalBias = 0f)
+                                            )
+                                        )
+                                    }
+                                }
+                                Spacer(Modifier.height(12.dp))
+                                LinearWavyProgressIndicator(
+                                    progress = { progressAnim.value },
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+                        }
+                    } else {
+                        RvKernelManagerNavHost()
+                    }
+                }
             }
         } else {
             Surface(
