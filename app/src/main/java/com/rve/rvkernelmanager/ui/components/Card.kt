ec8dcf15748be2132044084d673a5a512d6c4f78
Rve27 <rve27github@gmail.com>
2026-01-22 07:12:04 +0700

    ui: components: Introduces a new `ExpandableCard` composable
    Signed-off-by: Rve27 <rve27github@gmail.com>

---
+++ app/src/main/java/com/rve/rvkernelmanager/ui/components/Card.kt
@@ -0,0 +1,96 @@
+@file:OptIn(ExperimentalMaterial3ExpressiveApi::class)
+
+package com.rve.rvkernelmanager.ui.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.composables.icons.materialsymbols.roundedfilled.R.drawable.materialsymbols_ic_expand_more_rounded_filled
+
+object Card {
+    @Composable
+    fun ExpandableCard(
+        icon: Any?,
+        text: String,
+        onClick: () -> Unit,
+        expanded: Boolean,
+        content: @Composable () -> Unit
+    ) {
+        val rotateArrow by animateFloatAsState(
+            targetValue = if (expanded) 180f else 0f,
+            animationSpec = MaterialTheme.motionScheme.slowSpatialSpec(),
+        )
+
+        Card(
+            onClick = onClick,
+            shape = MaterialTheme.shapes.extraLarge,
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceBright,
+                contentColor = MaterialTheme.colorScheme.onSurface,
+            )
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                        .padding(8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    when (icon) {
+                        is ImageVector -> Icon(
+                            imageVector = icon,
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                            contentDescription = null
+                        )
+
+                        is Painter -> Icon(
+                            painter = icon,
+                            tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                            contentDescription = null
+                        )
+                    }
+                }
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.weight(1f),
+                )
+                Icon(
+                    painter = painterResource(materialsymbols_ic_expand_more_rounded_filled),
+                    tint = MaterialTheme.colorScheme.onSurface,
+                    contentDescription = if (expanded) "Expanded" else "Collapsed",
+                    modifier = Modifier.rotate(rotateArrow),
+                )
+            }
+            content()
+        }
+    }
+}
