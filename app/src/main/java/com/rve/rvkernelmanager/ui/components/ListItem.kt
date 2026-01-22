04fc75ed02482c341f29165c1bc0b11cad8fc40d
Rve27 <rve27github@gmail.com>
2026-01-22 07:51:33 +0700

    ui: components: Rename ListItemCard to ItemCard
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/rvkernelmanager/ui/components/ListItem.kt
+++ app/src/main/java/com/rve/rvkernelmanager/ui/components/ListItem.kt
@@ -20,18 +20,12 @@ package com.rve.rvkernelmanager.ui.components
 
 import androidx.annotation.StringRes
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -39,7 +33,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -131,75 +124,6 @@ fun CustomListItem(
                     },
                     color = summaryColor ?: MaterialTheme.colorScheme.onSurfaceVariant,
                     modifier = if (animateContentSize) Modifier.animateContentSize() else Modifier,
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun ListItemCard(
-    icon: Any?,
-    title: String,
-    body: String,
-    onClick: (() -> Unit)?,
-    onLongClick: (() -> Unit)?,
-
-) {
-    Card(
-        modifier = Modifier
-            .clip(MaterialTheme.shapes.extraLarge)
-            .combinedClickable(
-                onClick = onClick ?: {},
-                onLongClick = onLongClick,
-            ),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceBright,
-            contentColor = MaterialTheme.colorScheme.onSurface,
-        )
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .padding(8.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                when (icon) {
-                    is ImageVector -> Icon(
-                        imageVector = icon,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                        contentDescription = null
-                    )
-
-                    is Painter -> Icon(
-                        painter = icon,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                        contentDescription = null
-                    )
-                }
-            }
-            Column(
-                modifier = Modifier.animateContentSize(
-                    animationSpec = MaterialTheme.motionScheme.slowSpatialSpec()
-                ),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.titleMedium,
-                )
-                Text(
-                    text = body,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
             }
         }
